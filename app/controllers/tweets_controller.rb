b37c717c9c87adbbd92aae78440521c7dd149686
tomoko matsuo <matsuo09212001@yahoo.co.jp>
2020-04-01 14:59:26 +0900

    確認画面、投稿画面の打ち込んだ内容の保持完了

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,21 +3,30 @@ class TweetsController < ApplicationController
   def index
     @tweets = Tweet.all
   end
+
   def new
     @tweet = Tweet.new
   end
+
   def create
     @tweet = Tweet.new(tweet_params)
-    if @tweet.save
-      redirect_to tweets_path, notise: "投稿を作成しました！"
-    else
+    if params[:back]
       render :new
+    else
+     if @tweet.save
+      redirect_to tweets_path, notise: "投稿を作成しました！"
+     else
+      render :new
+     end
     end
   end
+
   def show
   end
+
   def edit
   end
+
   def update
     if @tweet.update(tweet_params)
       redirect_to tweets_path, notice: "ブログを編集しました！"
@@ -25,15 +34,24 @@ class TweetsController < ApplicationController
       render :edit
     end
   end
+
   def destroy
     @tweet.destroy
     redirect_to tweets_path, notice: "ブログを削除しました！"
   end
+
+  def confirm 
+    @tweet = Tweet.new(tweet_params)
+    render :new if @tweet.invalid?
+  end
+
   private
   def tweet_params
     params.require(:tweet).permit(:content)
   end
+
   def set_tweet
     @tweet = Tweet.find(params[:id])
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get '/tweets', to: 'tweets#index'
-  resources :tweets
-  
+  resources :tweets do
+    collection do
+      post :confirm
+    end
+  end
 end
