6a89626f47f9f52e539b85c5e888811e319ba093
tomoko matsuo <matsuo09212001@yahoo.co.jp>
2020-04-01 18:28:26 +0900

    タイムゾーン設定完了

--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,8 @@ ActiveRecord::Schema.define(version: 2020_03_31_094104) do
 
   create_table "tweets", force: :cascade do |t|
     t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
